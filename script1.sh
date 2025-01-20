ebe31eb58fe36e85f7799cb3c9dc61a40f498d20
Nasar <71958077+Abu103@users.noreply.github.com>
2025-01-20 12:00:29 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-if [ -f recovery.img.lz4 ];then
-	lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
+# Check if the .lz4 file exists and unpack if so
+if [ -f recovery.img.lz4 ]; then
+    lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
 fi
 
-off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
+# Find the offset for SEANDROIDENFORCE and split the image
+off=$(grep -ab -o SEANDROIDENFORCE recovery.img | tail -n 1 | cut -d : -f 1)
 dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
 
-if [ ! -f phh.pem ];then
+# If the private key doesn't exist, generate it
+if [ ! -f phh.pem ]; then
     openssl genrsa -f4 -out phh.pem 4096
 fi
